ca302d197afe570b1ce613a636757102b9c05003
DariaKomissarova <litovchenkodarya@gmail.com>
2025-02-14 23:05:23 +0100

    update Bundlefile

--- Brewfile
+++ Brewfile
@@ -1,6 +1,4 @@
 # Taps
-tap 'homebrew/cask-fonts'
-tap 'homebrew/cask-versions'
 tap 'homebrew/bundle'
 tap 'stripe/stripe-cli'
 
@@ -14,23 +12,28 @@ brew 'httpie'
 brew 'mas' # Mac App Store manager
 brew 'pkg-config' # https://github.com/driesvints/dotfiles/issues/20
 brew 'svn' # Needed to install fonts
-brew zsh-autosuggestions
-brew zsh-syntax-highlighting
 
 # Development
 # brew 'imagemagick'
 brew 'yarn'
+brew 'direnv'
+brew 'pyenv'
+brew 'neovim'
+brew 'python'
+brew 'shellcheck'
+brew 'postgresql@16'
+brew 'pipx'
 
 # Apps
 cask 'docker'
 cask 'github'
 cask 'google-chrome'
 cask 'httpie' # interface to do API requests
-# cask 'loom' # Free screen recorder for Mac and PC
 cask 'telegram-desktop'
 cask 'the-unarchiver'
 cask 'visual-studio-code'
 cask 'zoom'
+cask '1password'
 
 # Fonts
 cask 'font-lato'
@@ -38,5 +41,3 @@ cask 'font-open-sans'
 cask 'font-roboto'
 cask 'font-source-code-pro-for-powerline'
 cask 'font-source-code-pro'
-cask 'font-source-sans-pro'
-cask 'font-source-serif-pro'
--- fresh.sh
+++ fresh.sh
@@ -7,12 +7,16 @@
 echo "Setting up your Mac..."
 
 # Check for Oh My Zsh and install if we don't have it
-if test ! $(which omz); then
+echo "checking oh-my-zsh is there.."
+if test $ZSH = $HOME/.oh-my-zsh ; then
+  echo "installing oh-my-zsh..."
   /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
 fi
 
+echo "Checking Brew..."
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
+  echo "installing Brew"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
   echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
@@ -27,9 +31,10 @@ ln -sw $HOME/.dotfiles/.zshrc $HOME/.zshrc
 brew update
 
 # Install all our dependencies with bundle (See Brewfile)
+echo "installing libraries with brew.."
 brew tap homebrew/bundle
 brew bundle --file ./Brewfile
 
 # link the wezterm config file with th one in the dotfiles
-rm $HOME/.config/wezterm/wezterm.lua 
+rm $HOME/.config/wezterm/wezterm.lua
 ln -sw $HOME/.dotfiles/wezterm/wezterm.lua $HOME/.config/wezterm/wezterm.lua
